aeff92bc02190580dba01fb49872578196d4a59d
Aditya Shinde <shinde.aditya386@gmail.com>
2018-05-19 02:40:50 -0400

    Debugging trainer

--- Segmentation/code/trainer.py
+++ Segmentation/code/trainer.py
@@ -2,18 +2,24 @@ import torch
 import torch.nn.functional as F
 import torch.optim as O
 import torch.autograd as A
+from torch.utils.data import DataLoader
 import logging
 
 
 class Trainer:
 
     def __init__(self, dataset, model):
-        self.dataset = dataset
+        self.dataset = DataLoader(dataset,
+                                  shuffle=True,
+                                  batch_size=1,
+                                  num_workers=2)
         self.model = model
         self.logger = logging.getLogger(f"{self.__class__.__name__}")
+        self.device = "cuda:0"
 
     def train(self, epochs=10):
         # Put model in training mode.
+        self.model.to(self.device)
         self.model.train()
         self.logger.info("Model put in training mode.")
 
@@ -23,9 +29,10 @@ class Trainer:
                 _sample = self.dataset[sample]
 
                 _in, _out = _sample
-                t_in, t_out = torch.from_numpy(_in), torch.from_numpy(_out)
+                print(_in.shape)
+                #t_in, t_out = torch.from_numpy(_in), torch.from_numpy(_out)
 
-                prediction = self.model(A.Variable(t_in))
-                print(prediction.size())
+                #prediction = self.model(A.Variable(t_in))
+                #print(prediction.size())
 
                 break
