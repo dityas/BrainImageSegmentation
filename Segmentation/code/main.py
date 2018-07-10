8aa4f9a7941c68c36bc8371626ce42f305a57186
Aditya Shinde <shinde.aditya386@gmail.com>
2018-07-10 03:46:51 -0400

    Track every 500 batches

--- Segmentation/code/main.py
+++ Segmentation/code/main.py
@@ -84,4 +84,4 @@ pipeline = SegmentationPipeline(training_set=train_dataset,
                                 device=device,
                                 metric=dice_score)
 
-pipeline.train(epochs=20, track_every=100)
+pipeline.train(epochs=20, track_every=500)
