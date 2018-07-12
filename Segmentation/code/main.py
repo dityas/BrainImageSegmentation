dc228405a2405d4f2ab35d8a8a1eb05b8babed05
Aditya Shinde <shinde.aditya386@gmail.com>
2018-07-12 00:16:44 -0400

    Batch2

--- Segmentation/code/main.py
+++ Segmentation/code/main.py
@@ -32,7 +32,7 @@ train_dataset = DataLoader(train_dataset,
 
 val_dataset = DataLoader(val_dataset,
                          shuffle=True,
-                         batch_size=1,
+                         batch_size=2,
                          num_workers=1,
                          drop_last=True)
 
